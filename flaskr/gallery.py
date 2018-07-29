6f0aa7d71ac1253d27b11782742a34602ac4d2a2
jakehappersett <jake.happersett@gmail.com>
2018-07-29 10:51:17 -0400

    reverted gallery and used jinja2 built in list slicing to assemble row groups

--- flaskr/gallery.py
+++ flaskr/gallery.py
@@ -9,7 +9,7 @@ bp = Blueprint('gallery', __name__, url_prefix='/gallery')
 def get_image_paths():
     #todo: convert to dictionary that stores type based on folder input
     images = os.listdir(current_app.config['UPLOAD_FOLDER'])
-    full = deque()
+    full = []
     for image in images:
         if image.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']:
                 full.append(image)
